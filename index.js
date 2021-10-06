19b80890c8f1a2f313addec7e4b670014a789ead
Kristen Picard <kristenpicard93@gmail.com>
2021-10-06 13:24:00 -0500

    Now displays lines based on number of letters in random word chosen

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+// Get a random word from a list
+
 let wordList = ["First word", "Second Word", "Third Word"];
 
 function getRandomWord(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
-console.log(getRandomWord(wordList));
+
+let chosenWord = getRandomWord(wordList);
+
+// Display # of lines based on letters in word chosen
+
+let arrayOfLetters = chosenWord.split("");
+
+console.log(arrayOfLetters);
+
+let letterLines = document.getElementById("letterLines");
+
+function displayLinesBasedOnLetterNums(characters) {
+  for (i = 0; i < characters.length; i++) {
+    letterLines.innerHTML += " ";
+    if (characters[i] == " ") {
+      letterLines.innerHTML += "&nbsp;&nbsp;";
+    } else {
+      letterLines.innerHTML += "_";
+    }
+  }
+}
+
+displayLinesBasedOnLetterNums(arrayOfLetters);
